ab0e1387e316371ac61b45a02952a6ddc010b90c
Bernard411 <hamiebernard@gmail.com>
2025-09-04 14:18:21 -0500

    update

--- nyasabox/core/templatetags/music_filters.py
+++ nyasabox/core/templatetags/music_filters.py
@@ -8,3 +8,14 @@ def div(value, arg):
         return float(value) / float(arg)
     except (ValueError, ZeroDivisionError):
         return 0
+
+from django import template
+
+register = template.Library()
+
+@register.filter
+def div(value, arg):
+    try:
+        return float(value) / float(arg)
+    except (ValueError, ZeroDivisionError):
+        return 0
